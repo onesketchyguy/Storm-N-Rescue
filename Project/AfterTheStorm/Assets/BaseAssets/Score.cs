42c7d3aa612d56b5a6796c20afa326e6b58bfe12
Rattl3bones <42248595+Rattl3bones@users.noreply.github.com>
2019-06-14 15:37:42 -0400

    Score holding script

---
+++ Project/AfterTheStorm/Assets/BaseAssets/Score.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    
+    public static int score; //The player's score
+
+   
+
+    void Start()
+    {
+        score = 0;
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    public static void Reset()
+    {
+        score = 0;
+    }
+
+    public static void ModifyScore(int mod)
+    {
+        score = score + mod;
+    }
+
+
+
+
+}
