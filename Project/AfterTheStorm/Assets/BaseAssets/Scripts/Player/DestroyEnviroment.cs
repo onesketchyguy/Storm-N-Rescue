8b575170485ae4a50537e557859a57d1a59ad260
onesketchyguy <34846360+onesketchyguy@users.noreply.github.com>
2019-06-14 15:30:59 -0700

    Created Game Scene

---
+++ Project/AfterTheStorm/Assets/BaseAssets/Scripts/Player/DestroyEnviroment.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace LowEngine
+{
+    using Helpers;
+
+    public class DestroyEnviroment : MonoBehaviour, ICombat
+    {
+        public Vector4 input { get; set; }
+        public bool attacking { get; set; }
+
+        private void Update()
+        {
+        }
+    }
+}
