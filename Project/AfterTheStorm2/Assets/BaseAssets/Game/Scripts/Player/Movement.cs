6c594f952f37c4ca9c91c2c9ca75ab9e4766ee9a
onesketchyguy <34846360+onesketchyguy@users.noreply.github.com>
2020-03-19 21:57:17 -0700

    Updated camera, and movement

---
+++ Project/AfterTheStorm2/Assets/BaseAssets/Game/Scripts/Player/Movement.cs
@@ -0,0 +1,67 @@
+﻿using LowEngine.Audio;
+using LowEngine.Helpers;
+using UnityEngine;
+
+namespace LowEngine
+{
+    /// <summary>
+    /// Allows for both arial and ground movement, including a ground pound, and can feed fall damage.
+    /// </summary>
+    public class Movement : MonoBehaviour, IMovement
+    {
+        #region IMovement items
+
+        public Vector4 input { get; set; }
+
+        public Vector2 maxSpeed { get; private set; }
+        public Vector2 currentSpeed { get; set; }
+        public TakeDamage fallDamageCallback { get; set; }
+
+        #endregion IMovement items
+
+        private Vector4 lastInput;
+
+        private new Rigidbody rigidbody;
+
+        // Inspector items
+        public float speed = 5.5f;
+
+        private bool movingRight = true;
+
+        private void Start()
+        {
+            rigidbody = GetComponent<Rigidbody>() ?? gameObject.AddComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            UpdatePosition();
+        }
+
+        private void UpdatePosition()
+        {
+            var velocity = rigidbody.velocity;
+
+            velocity -= (Vector3)lastInput;
+
+            // Add the user input
+            var xMove = (input.x * speed);
+            velocity += new Vector3(xMove, 0, 0);
+
+            // Move in that direction (visual only)
+            var leanAmount = ((-input.x * 2) + -currentSpeed.x);
+            rigidbody.rotation = Quaternion.Euler(transform.forward * leanAmount * Time.deltaTime);
+
+            // Face in the direction we are moving (visual kinda stuff)
+            if (input.x != 0)
+                movingRight = (velocity.x > 0);
+
+            transform.localScale = new Vector3(movingRight ? 1 : -1, 1, 1);
+
+            // Set the variables
+            rigidbody.velocity = currentSpeed = velocity;
+
+            lastInput = new Vector4(xMove, 0, 0);
+        }
+    }
+}
---
+++ Project/AfterTheStorm2/Assets/BaseAssets/MainMenu/Scripts/ScaleTween.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace MainMenu
+{
+    public class ScaleTween : MonoBehaviour
+    {
+        public LeanTweenType easeType;
+        public AnimationCurve curve;
+
+        public float duration = 0.5f;
+
+        public float delay;
+
+        private Vector3 scale;
+
+        private void OnEnable()
+        {
+            if (scale == Vector3.zero)
+            {
+                scale = transform.localScale;
+
+                LeanTween.scale(gameObject, Vector3.zero, 0);
+            }
+            else
+            {
+                if (easeType == LeanTweenType.animationCurve)
+                    LeanTween.scale(gameObject, scale, duration).setDelay(delay).setEase(curve);
+                else LeanTween.scale(gameObject, scale, duration).setDelay(delay).setEase(easeType);
+            }
+        }
+    }
+}
